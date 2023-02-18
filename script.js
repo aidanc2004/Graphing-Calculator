41042430e3f71557d6cb278941e16c0ceae640ac
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-02-18 05:54:58 -0400

    Add comments and fix x/y scale absolute values

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 let graph = document.getElementById("graph");
 let ctx = graph.getContext("2d");
 
+// width and height of the canvas
 let width = 800;
 let height = 600;
 
-let xmin = -5;
-let xmax = 5;
-
-let ymin = 0;
+// range of x values
+let xmin = -10;
+let xmax = 10;
+// range of y values
+let ymin = -10;
 let ymax = 10;
 
-// simple function
-let f = x => Math.pow(x, 2);
-//let g = x => Math.sin(x)
+// test functions
+//let f = x => Math.pow(x, 2);
+let f = x => Math.sin(x) * 5 + 10;
+//let f = x => (x + 1) / (x - 1); // problems with horizontal asymptotes
 
-let xscale = width / (Math.abs(xmin) + xmax);
-let yscale = height / (ymin + ymax)
+// scale of x and y values to match width and height of canvas
+let xscale = width / (Math.abs(xmin) + Math.abs(xmax));
+let yscale = height / (Math.abs(ymin) + Math.abs(ymax))
 
 ctx.lineWidth = 1;
 
+// begin drawing functions
 ctx.beginPath();
 for (let i = xmin; i < xmax; i += 0.1) {
-    let x = i * xscale + width / 2;
-    let y = (-f(i) * yscale + height);
+    let x = i * xscale + width / 2; // calculate x value
+    let y = (-f(i) * yscale + height); // calculate y value
 
-    ctx.lineTo(x, y);
+    ctx.lineTo(x, y); // draw line to point
 }
 ctx.stroke();
