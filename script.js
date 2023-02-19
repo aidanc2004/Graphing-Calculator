581633ea26624826525abfb4bc0573d8f76546e3
aidanc2004 <67815343+aidanc2004@users.noreply.github.com>
2023-02-19 10:17:25 -0400

    Add x and y axis

--- script.js
+++ script.js
@@ -12,25 +12,51 @@ let xmax = 5;
 let ymin = -5;
 let ymax = 5;
 
+// draw the x and y axis
+function drawAxes() {
+    // position on the graph where x/y = 0
+    let xZero = transformRange(0, {min: ymin, max: ymax}, {min: 0, max: height});
+    let yZero = transformRange(0, {min: xmin, max: xmax}, {min: 0, max: width});
+
+    // draw x axis
+    ctx.beginPath();
+    ctx.setLineDash([5, 10]);
+    ctx.moveTo(0, xZero);
+    ctx.lineTo(width, xZero);
+    ctx.stroke();
+    ctx.closePath();
+    
+    // draw y axis
+    ctx.beginPath();
+    ctx.moveTo(yZero, 0);
+    ctx.lineTo(yZero, height);
+    ctx.stroke();
+    ctx.closePath();
+    ctx.setLineDash([]);
+}
+
+// used to scale of x and y values to match width and height of canvas
+function transformRange(value, r1, r2) {
+    var scale = (r2.max - r2.min) / (r1.max - r1.min);
+    return (value - r1.min) * scale;
+}
+
 // test functions
 let f = x => Math.pow(x, 2);
 //let f = x => -Math.pow(x, 2);
 //let f = x => (5 * Math.sin(x));
 //let f = x => (x + 1) / (x - 1); // problems with horizontal asymptotes
 
-// used to scale of x and y values to match width and height of canvas
-var transformRange = (value, r1, r2) => {
-    var scale = (r2.max - r2.min) / (r1.max - r1.min);
-    return (value - r1.min) * scale;
-}
-
 ctx.lineWidth = 1;
-
-// begin drawing functions
-ctx.beginPath();
 
 ctx.scale(1,-1); // flip graph upside down so 0 is on the bottom
 ctx.translate(0, -height); // fix position
+
+// draw x and y axis
+drawAxes();
+
+// begin drawing functions
+ctx.beginPath();
 
 for (let i = xmin; i < xmax; i += 0.01) {
     let x = transformRange(i, {min: xmin, max: xmax}, {min: 0, max: width});
